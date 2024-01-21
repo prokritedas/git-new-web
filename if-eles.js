ad4bdd7c6b1ca913051965b4fee861d7c0909196
prokritedas <prokritedas@gmail.com>
2024-01-21 18:46:36 +0600

    first commit

---
+++ if-eles.js
@@ -0,0 +1,51 @@
+
+//if,else example
+
+let a= 20;
+let b=40;
+
+if(a>b){
+    console.log("b is less than a");
+}
+
+else{
+    console.log("b is not less than a");
+}
+
+
+
+//if,else,else if
+let c = 70;
+let d= 20;
+
+if(c== d){
+    console.log("c,d are equal");
+}
+
+else if(c<d){
+    console.log("d is more than c");
+}
+ 
+else if(c>d){
+    console.log("c is more than d");
+}
+  else{
+    console.log("c and d are not equal");
+  }
+
+
+  //switch 
+let p=50;
+
+switch(p){
+    case 70:
+        console.log("70 is right");
+break
+
+case 40:
+    console.log("40 is correct");
+    break
+
+    default:
+    console.log("this is a default ans");
+}
